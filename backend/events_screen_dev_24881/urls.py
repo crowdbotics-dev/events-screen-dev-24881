13abbff7d12d22cd061b7be04fea4e914f9cd15f
Crowdbotics <engineering@crowdbotics.com>
2022-06-23 13:08:23 +0000

    Update admin title

--- backend/events_screen_dev_24881/urls.py
+++ backend/events_screen_dev_24881/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "events-screen"
-admin.site.site_title = "events-screen Admin Portal"
-admin.site.index_title = "events-screen Admin"
+admin.site.site_header = "user-profile-screen"
+admin.site.site_title = "user-profile-screen Admin Portal"
+admin.site.index_title = "user-profile-screen Admin"
 
 # swagger
 api_info = openapi.Info(
